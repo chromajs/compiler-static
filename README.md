4a27fc983ecd8f2d68690c308ebdd54bc80d351b
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2023-03-04 21:45:50 -0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # chroma-html-compiler
+
 Chroma's Markdown HTML compiler
+
+# Installation/Usage
+```bash
+npx chroma-html-compiler -h
+```
+
+**chroma-html-compiler** expects two arguments:
+
+- --srcPath (-s) which defines the directory to compile HTML files from
+- --outPath (-o) which defines the directory to output compiled HTML files and other project dependencies
+
+When running Chroma HTML Compiler, Chroma will keep the directory structure of your project,
+it will also keep all additional files in the directory that are not HTML files (ie. js, css, ect.) and
+after it will add compiled HTML files in the correct directory structure
+
+# Warnings
+**HTML files must use tabs as there spacing, not spaces! (Will be fixed soon)**
+
+**Tabs are suppressed, so using tab code blocks does not work! (Will be fixed soon)**
+
+**Markdown is only recognized inside \<chrome>\</chrome> tags! (Will be fixed soon)**
+
+**Markdown is effected by CSS!**
+
+# Example
+in/test.html
+```html
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<chroma># Hello World!</chroma>
+	</body>
+</html>
+```
+
+```bash
+npx chroma-html-compiler -s in/ -o out/
+```
+
+out/test.html
+```html
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<h1 id="hello-world">Hello World!</h1>
+	</body>
+</html>
+```
---
+++ bin/index.ts
@@ -0,0 +1,121 @@
+#! /usr/bin/env node
+
+import { parse } from "ts-command-line-args";
+import { resolve, join } from "path";
+import { existsSync, rmSync, mkdirSync, readdirSync, statSync, copyFileSync, readFileSync, writeFileSync } from "fs";
+import { marked } from "marked";
+import dompurify from "dompurify";
+import { JSDOM } from "jsdom";
+
+const window = new JSDOM("").window;
+const purify = dompurify(window as unknown as Window);
+
+const chromaRegex = /<chroma>([^]*?)<\/chroma>/gi;
+
+function getPaths(dirPath: string) {
+    let filePaths: string[] = [];
+
+    try {
+        readdirSync(dirPath).forEach(file => {
+            if (statSync(join(dirPath, "/", file)).isDirectory()) {
+                filePaths = filePaths.concat(getPaths(dirPath + "/" + file));
+            } else {
+                const absolutePath = resolve(dirPath, file);
+                filePaths.push(absolutePath);
+            }
+        });
+    } catch (e) {
+        console.log("Failed! Failed to read directory!");
+        process.exit(1);
+    }
+
+    return filePaths;
+}
+
+interface CLIArgs {
+    srcPath: string
+    outPath: string
+    help?: boolean
+}
+
+const args = parse<CLIArgs>(
+    {
+        srcPath: { type: String, alias: "s", description: "The source folder of your Chroma project" },
+        outPath: { type: String, alias: "o", description: "The out folder of your compiled Chroma project" },
+        help: { type: Boolean, optional: true, alias: "h", description: "Prints this message" },
+    },
+    {
+        helpArg: "help",
+        headerContentSections: [{ header: "Chroma HTML Compiler Help", content: "Thanks for supporting us - ChromaJS team" }],
+        footerContentSections: [{ header: "Chroma HTML Compiler Help", content: `Copyright © ${new Date().getFullYear()} Chroma` }],
+    },
+);
+
+if (!args.help) {
+    if (!existsSync(args.srcPath)) {
+        console.log("Failed! SRC Path must exist!");
+        process.exit(1);
+    }
+
+    if (existsSync(args.outPath)) {
+        try {
+            rmSync(args.outPath, { recursive: true, force: true });
+        } catch (e) {
+            console.log("Failed! Failed to delete OUT Path");
+            process.exit(1);
+        }
+    }
+
+    try {
+        mkdirSync(args.outPath);
+    } catch (e) {
+        console.log("Failed! Failed to create directory OUT Path");
+        process.exit(1);
+    }
+
+    const rootDir = resolve(args.srcPath);
+    const files = getPaths(args.srcPath);
+    const htmlFiles = files.filter(file => file.match(/.html$/));
+    const otherFiles = files.filter(file => !file.match(/.html$/));
+
+    otherFiles.map(file => {
+        const relativePath = file.split(rootDir)[1];
+        const relativeSplit = relativePath.split("\\");
+
+        if (relativeSplit.length > 2) {
+            mkdirSync(join(args.outPath, relativeSplit[relativeSplit.length - 2]));
+        }
+
+        copyFileSync(file, join(args.outPath, relativePath));
+    });
+
+    htmlFiles.map(file => {
+        const relativePath = file.split(rootDir)[1];
+        const relativeSplit = relativePath.split("\\");
+
+        if (relativeSplit.length > 2) {
+            mkdirSync(join(args.outPath, relativeSplit[relativeSplit.length - 2]));
+        }
+
+        let content = readFileSync(file, "utf-8");
+        const code = [...content.matchAll(chromaRegex)];
+
+        code.map((chromaReg) => {
+            const chromaString = chromaReg[0];
+
+            while (content.indexOf(chromaString) !== -1) {
+                const noTag = chromaString
+                    .replace(/<chroma>/gi, "")
+                    .replace(/<\/chroma>/gi, "")
+                    .replace(/\t/g, "");
+
+                content = content.replace(
+                    chromaString,
+                    purify.sanitize(marked(noTag, { async: false }))
+                );
+            }
+        });
+
+        writeFileSync(join(args.outPath, relativePath), content, "utf-8");
+    });
+}
