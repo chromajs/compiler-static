17832877187e312f803b44b3ee23dbada6c91533
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2023-03-05 13:53:42 -0800

    v1.0.1

--- README.md
+++ README.md
@@ -3,35 +3,51 @@
 Chroma's Markdown HTML compiler
 
 # Installation/Usage
+
 ```bash
 npx chroma-html-compiler -h
 ```
 
-**chroma-html-compiler** expects two arguments:
+**chroma-html-compiler** expects two arguments and has one optional argument:
 
-- --srcPath (-s) which defines the directory to compile HTML files from
-- --outPath (-o) which defines the directory to output compiled HTML files and other project dependencies
+-   --srcPath (-s) which defines the directory to compile HTML files from
+-   --outPath (-o) which defines the directory to output compiled HTML files and other project dependencies
+-   [--tabSpace (-t)] which defines how many spaces there are per tab. Defaults to 4.
 
 When running Chroma HTML Compiler, Chroma will keep the directory structure of your project,
 it will also keep all additional files in the directory that are not HTML files (ie. js, css, ect.) and
 after it will add compiled HTML files in the correct directory structure
 
 # Warnings
-**HTML files must use tabs as there spacing, not spaces! (Will be fixed soon)**
-
-**Tabs are suppressed, so using tab code blocks does not work! (Will be fixed soon)**
-
-**Markdown is only recognized inside \<chrome>\</chrome> tags! (Will be fixed soon)**
 
 **Markdown is effected by CSS!**
 
+# Clause Code
+
+In Chroma, there is something called the Clause Code. The clause code comes at the start of every chroma tag (except for src chroma tags)
+and it tells the compiler the correct spacing of your program. This is needed because Markdown is whitespace sensitive so the character "|"
+at the start of your Chroma tags tells the compiler the correct spacing of your program.
+
+**|**
+
+# Chroma Tags
+
+There are two types of Chroma tags, normal Chroma tags, and SRC Chroma tags. Normal Chroma tags act just like a Markdown language in your HTML,
+you type Markdown in the tags (with a Clause Code) and it will compile it at build time.
+
+SRC Chroma tags are self closing tags that allow you to put a "src='Markdown file location here'" in the tag and at build time, the tag
+will be replaced with the rendered contents of your Markdown file.
+
 # Example
+
 in/test.html
+
 ```html
 <!DOCTYPE html>
 <html>
 	<head></head>
 	<body>
+		|
 		<chroma># Hello World!</chroma>
 	</body>
 </html>
@@ -42,6 +58,43 @@ npx chroma-html-compiler -s in/ -o out/
 ```
 
 out/test.html
+
+```html
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<h1 id="hello-world">Hello World!</h1>
+	</body>
+</html>
+```
+
+# Example 2
+
+in/test.html
+
+```html
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<chroma src="./in/test.md" />
+	</body>
+</html>
+```
+
+in/test.md
+
+```markdown
+# Hello World!
+```
+
+```bash
+npx chroma-html-compiler -s in/ -o out/
+```
+
+out/test.html
+
 ```html
 <!DOCTYPE html>
 <html>
--- bin/index.ts
+++ bin/index.ts
@@ -6,11 +6,13 @@ import { existsSync, rmSync, mkdirSync, readdirSync, statSync, copyFileSync, rea
 import { marked } from "marked";
 import dompurify from "dompurify";
 import { JSDOM } from "jsdom";
+import { minify } from "html-minifier";
 
 const window = new JSDOM("").window;
 const purify = dompurify(window as unknown as Window);
 
 const chromaRegex = /<chroma>([^]*?)<\/chroma>/gi;
+const chromaSrcRegex = /<chroma src="([^]*?)"([^]*?)>/gi;
 
 function getPaths(dirPath: string) {
     let filePaths: string[] = [];
@@ -35,6 +37,7 @@ function getPaths(dirPath: string) {
 interface CLIArgs {
     srcPath: string
     outPath: string
+    tabSpace?: number
     help?: boolean
 }
 
@@ -42,6 +45,7 @@ const args = parse<CLIArgs>(
     {
         srcPath: { type: String, alias: "s", description: "The source folder of your Chroma project" },
         outPath: { type: String, alias: "o", description: "The out folder of your compiled Chroma project" },
+        tabSpace: { type: Number, alias: "t", optional: true, description: "Markdown is white space sensative so we need to know the tab size of your file. Default: 4", defaultValue: 4 },
         help: { type: Boolean, optional: true, alias: "h", description: "Prints this message" },
     },
     {
@@ -52,6 +56,11 @@ const args = parse<CLIArgs>(
 );
 
 if (!args.help) {
+    let tabsToSpaces = "";
+    for (let i = 0; i < args.tabSpace!; i++) {
+        tabsToSpaces += " ";
+    }
+
     if (!existsSync(args.srcPath)) {
         console.log("Failed! SRC Path must exist!");
         process.exit(1);
@@ -98,7 +107,28 @@ if (!args.help) {
         }
 
         let content = readFileSync(file, "utf-8");
+        content = minify(content, {
+            removeComments: true,
+        });
+
+        const pre = [...content.matchAll(chromaSrcRegex)];
         const code = [...content.matchAll(chromaRegex)];
+
+        pre.map(chromaReg => {
+            const chromaString = chromaReg[0];
+            let md;
+            try {
+                md = readFileSync((chromaString.match(/"([^]*?)"/) as RegExpMatchArray)[0].split("\"")[1], "utf-8");
+            } catch (e) {
+                console.log("[Chroma] Failed to compile! Chroma SRC Markdown File does not exist!");
+                process.exit(1);
+            }
+
+            content = content.replace(
+                chromaString,
+                purify.sanitize(marked(md, { async: false }))
+            );
+        });
 
         code.map((chromaReg) => {
             const chromaString = chromaReg[0];
@@ -107,11 +137,70 @@ if (!args.help) {
                 const noTag = chromaString
                     .replace(/<chroma>/gi, "")
                     .replace(/<\/chroma>/gi, "")
-                    .replace(/\t/g, "");
+                    .replace(/\t/gi, tabsToSpaces);
+
+                let splitter = "\n";
+                if (noTag.includes("\r\n")) {
+                    splitter = "\r\n";
+                }
+
+                let lines = noTag.split(splitter);
+                let whiteSpace = 0;
+                let clauseCodeIndex;
+                for (let i = 0; i < lines.length; i++) {
+                    whiteSpace = 0;
+
+                    const line = lines[i];
+                    let found = false;
+                    for (let j = 0; j < line.length; j++) {
+                        if (line[j] === "|") {
+                            clauseCodeIndex = i;
+                            found = true;
+                            break;
+                        } else {
+                            whiteSpace++;
+                        }
+                    }
+
+                    if (found) {
+                        break;
+                    }
+                }
+
+                if (!clauseCodeIndex) {
+                    console.log(`[Chroma] Failed to compile! HTML File missing Clause Code: "|"`);
+                    process.exit(1);
+                }
+
+                lines = lines.slice(clauseCodeIndex + 1);
+                lines.map((line, index) => {
+                    let whiteSpaceCount = 0;
+                    let firstChar;
+
+                    for (let i = 0; i < line.length; i++) {
+                        if (!line[i].match(/ |\n|\r|\t/)) {
+                            firstChar = i;
+                            break;
+                        }
+                    }
+                    //console.log(firstChar);
+
+                    if (!firstChar) {
+                        firstChar = line.length - 1;
+                    }
+
+                    for (let i = 0; i < firstChar; i++) {
+                        if (whiteSpaceCount < whiteSpace && line[i].match(/ |\n|\r|\t/)) {
+                            whiteSpaceCount++;
+                        }
+                    }
+
+                    lines[index] = lines[index].slice(whiteSpaceCount);
+                });
 
                 content = content.replace(
                     chromaString,
-                    purify.sanitize(marked(noTag, { async: false }))
+                    purify.sanitize(marked(lines.join(splitter), { async: false }))
                 );
             }
         });
